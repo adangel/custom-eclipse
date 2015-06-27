ece3ffaa770720580af13b7813e0f70fd101824b
Andreas Dangel <adangel@users.sourceforge.net>
2015-06-27 23:33:32 +0200

    Make create-mirror.sh more independent. Move everything under temp.

--- create-mirror.sh
+++ create-mirror.sh
@@ -1,17 +1,33 @@
 #!/bin/bash
 
-#
-# Configuration:
-#
-# Base path to an eclipse installation. Note: This needs to be a prior 4.2 version! E.g. 3.7/indigo
-# http://www.eclipse.org/downloads/download.php?file=/eclipse/downloads/drops/R-3.7.2-201202080800/eclipse-SDK-3.7.2-linux-gtk-x86_64.tar.gz
-eclipse_home=$HOME/programs/eclipse-3.7.1-jee-indigo-SR1
+
+#####################
+# Preparation
+#####################
+mkdir -p temp
+
+# Download eclipse 3.7. Note: This needs to be prior 4.2 version, e.g. 3.7 indigo, as the later
+# versions don't have the code for accessing old update sites.
+eclipse_archive=temp/eclipse-SDK-3.7.2-linux-gtk-x86_64.tar.gz
+if [ ! -e $eclipse_archive ]; then
+    wget -O $eclipse_archive http://www.eclipse.org/downloads/download.php?file=/eclipse/downloads/drops/R-3.7.2-201202080800/eclipse-SDK-3.7.2-linux-gtk-x86_64.tar.gz
+fi
+
+eclipse_home=temp/eclipse-3.7.2
+if [ ! -d $eclipse_home ]; then
+   ( cd temp; tar xfzv ../$eclipse_archive; mv eclipse ../$eclipse_home )
+fi
+
 # Local directory for downloads (see download_convert*() and svn_site_checkout())
-downloads=./downloads/
+downloads=temp/downloads
 # Local directory for the old-style update site mirror
-site_mirror=./mirror-sites/
+site_mirror=temp/mirror-sites
 # Local p2 repo
-target_p2=file://$HOME/mirror-eclipse-mars/
+target_p2=file://`pwd`/temp/p2-mirror/
+
+###########################
+# Functions
+###########################
 
 #
 # Mirrors an old-style update site (not a p2 repository)
@@ -152,6 +168,15 @@ java -jar $eclipse_home/plugins/org.eclipse.equinox.launcher_*.jar \
    -publishArtifacts
 
 }
+
+
+
+
+#####################################
+# Downloads/Mirrors
+#####################################
+
+
 
 mirror_site "http://findbugs.cs.umd.edu/eclipse/"
 
