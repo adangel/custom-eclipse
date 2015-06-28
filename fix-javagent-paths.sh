3f5830b5650ab7e98e6081567d3870db80dbbdb9
Andreas Dangel <adangel@users.sourceforge.net>
2015-06-28 14:17:42 +0200

    Fix paths for macosx Not only eclipse.ini is in a different directory, also the relative path to the plugins folder is different

--- fix-javagent-paths.sh
+++ fix-javagent-paths.sh
@@ -25,16 +25,18 @@ for i in *.zip; do
     (
         cd ${i:0:20}*
         if [ -d Eclipse.app ]; then
-          eclipse_ini=Eclipse.app/Contents/MacOS/eclipse.ini
+          sed -i.backup "{
+                s/-javaagent:.\+\/\(plugins\/com\.zeroturnaround\.eclipse\.optimizer\.plugin_[0-9]\+\.[0-9]\+\.[0-9]\+\/agent\/eclipse-optimizer-agent.jar\)/-javaagent:..\/..\/..\/\1/
+                s/-javaagent:.\+\/\(plugins\/lombok_[0-9]\+\.[0-9]\+\.[0-9]\+\/target\/lib\/lombok.jar\)/-javaagent:..\/..\/..\/\1/
+          }" Eclipse.app/Contents/MacOS/eclipse.ini
         else
-          eclipse_ini=eclipse.ini
+          sed -i.backup "{
+                s/-javaagent:.\+\/\(plugins\/com\.zeroturnaround\.eclipse\.optimizer\.plugin_[0-9]\+\.[0-9]\+\.[0-9]\+\/agent\/eclipse-optimizer-agent.jar\)/-javaagent:\1/
+                s/-javaagent:.\+\/\(plugins\/lombok_[0-9]\+\.[0-9]\+\.[0-9]\+\/target\/lib\/lombok.jar\)/-javaagent:\1/
+          }" eclipse.ini
         fi
-
-        sed -i.backup "{
-            s/-javaagent:.\+\/\(plugins\/com\.zeroturnaround\.eclipse\.optimizer\.plugin_[0-9]\+\.[0-9]\+\.[0-9]\+\/agent\/eclipse-optimizer-agent.jar\)/-javaagent:\1/
-            s/-javaagent:.\+\/\(plugins\/lombok_[0-9]\+\.[0-9]\+\.[0-9]\+\/target\/lib\/lombok.jar\)/-javaagent:\1/
-        }" $eclipse_ini
     )
+
     zip -q -r $i ${i:0:20}*/
     rm -rf ${i:0:20}*/
 done
