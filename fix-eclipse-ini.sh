18c894ec19ac5f7163521bdc800d2111ea88e7f7
Andreas Dangel <adangel@users.sourceforge.net>
2016-07-04 20:32:50 +0200

    fix fix-eclipse-ini.sh to work with all files, update mac path

--- fix-eclipse-ini.sh
+++ fix-eclipse-ini.sh
@@ -7,7 +7,7 @@ for i in *.tar.gz; do
     tar xfz $i
     rm $i
     (
-        cd ${i:0:20}*
+        cd ${i:0:20}*[^zp]
         sed -i.backup "{
             s/-javaagent:.\+\/\(plugins\/com\.zeroturnaround\.eclipse\.optimizer\.plugin_[0-9]\+\.[0-9]\+\.[0-9]\+\/agent\/eclipse-optimizer-agent.jar\)/-javaagent:\1/
             s/-javaagent:.\+\/\(plugins\/lombok_[0-9]\+\.[0-9]\+\.[0-9]\+\/target\/lib\/lombok.jar\)/-javaagent:\1/
@@ -17,38 +17,48 @@ for i in *.tar.gz; do
             s/-showsplash\norg.eclipse.platform//
         }" eclipse.ini
     )
-    tar cfz $i ${i:0:20}*/
-    rm -rf ${i:0:20}*/
+    tar cfz $i ${i:0:20}*[^zp]/
+    rm -rf ${i:0:20}*[^zp]/
 done
 
-for i in *.zip; do
+for i in *macosx*.zip; do
     echo "Working on $i..."
     unzip -q $i
     rm $i
 
     (
-        cd ${i:0:20}*
-        if [ -d Contents ]; then
-          sed -i.backup "{
-                s/-javaagent:.\+\/\(plugins\/com\.zeroturnaround\.eclipse\.optimizer\.plugin_[0-9]\+\.[0-9]\+\.[0-9]\+\/agent\/eclipse-optimizer-agent.jar\)/-javaagent:..\/..\/..\/\1/
-                s/-javaagent:.\+\/\(plugins\/lombok_[0-9]\+\.[0-9]\+\.[0-9]\+\/target\/lib\/lombok.jar\)/-javaagent:..\/..\/..\/\1/
-          }" Contents/Eclipse/eclipse.ini
-          sed -i.backup "{
-              N
-              s/-showsplash\norg.eclipse.platform//
-          }" Contents/Eclipse/eclipse.ini
-        else
-          sed -i.backup "{
-                s/-javaagent:.\+\/\(plugins\/com\.zeroturnaround\.eclipse\.optimizer\.plugin_[0-9]\+\.[0-9]\+\.[0-9]\+\/agent\/eclipse-optimizer-agent.jar\)/-javaagent:\1/
-                s/-javaagent:.\+\/\(plugins\/lombok_[0-9]\+\.[0-9]\+\.[0-9]\+\/target\/lib\/lombok.jar\)/-javaagent:\1/
-          }" eclipse.ini
-          sed -i.backup "{
-              N
-              s/-showsplash\r?\norg.eclipse.platform//
-          }" eclipse.ini
-        fi
+        cd ${i:0:20}*.app
+        sed -i.backup "{
+            s/-javaagent:.\+\/\(plugins\/com\.zeroturnaround\.eclipse\.optimizer\.plugin_[0-9]\+\.[0-9]\+\.[0-9]\+\/agent\/eclipse-optimizer-agent.jar\)/-javaagent:..\/Eclipse\/\1/
+            s/-javaagent:.\+\/\(plugins\/lombok_[0-9]\+\.[0-9]\+\.[0-9]\+\/target\/lib\/lombok.jar\)/-javaagent:..\/Eclipse\/\1/
+        }" Contents/Eclipse/eclipse.ini
+        sed -i.backup "{
+            N
+            s/-showsplash\norg.eclipse.platform//
+        }" Contents/Eclipse/eclipse.ini
     )
 
-    zip -q -r $i ${i:0:20}*/
-    rm -rf ${i:0:20}*/
+    zip -q -r $i ${i:0:20}*.app/
+    rm -rf ${i:0:20}*.app/
+done
+
+for i in *win32*.zip; do
+    echo "Working on $i..."
+    unzip -q $i
+    rm $i
+
+    (
+        cd ${i:0:20}*[^zp]
+        sed -i.backup "{
+                s/-javaagent:.\+\/\(plugins\/com\.zeroturnaround\.eclipse\.optimizer\.plugin_[0-9]\+\.[0-9]\+\.[0-9]\+\/agent\/eclipse-optimizer-agent.jar\)/-javaagent:\1/
+                s/-javaagent:.\+\/\(plugins\/lombok_[0-9]\+\.[0-9]\+\.[0-9]\+\/target\/lib\/lombok.jar\)/-javaagent:\1/
+        }" eclipse.ini
+        sed -i.backup "{
+              N
+              s/-showsplash\r?\norg.eclipse.platform//
+        }" eclipse.ini
+    )
+
+    zip -q -r $i ${i:0:20}*[^zp]/
+    rm -rf ${i:0:20}*[^zp]/
 done
